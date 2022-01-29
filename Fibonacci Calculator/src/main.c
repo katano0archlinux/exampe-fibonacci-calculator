ae03210497f7a5cf6ed5fa1fcdbe86bc6a4290f0
katano_kamakashi <91697209+katano0archlinux@users.noreply.github.com>
2022-01-30 00:30:51 +0300

    Add files via upload

---
+++ Fibonacci Calculator/src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct numbers
+{
+	unsigned long int *n;
+};
+
+void calculateFibbonachi( struct numbers* number )
+{
+	unsigned long int a, b, index;
+	a = 0;
+	b = 1;
+	index = 1;
+
+	while ( a < *number->n )
+	{
+		b = a + b;
+		a = b - a;
+
+		if ( a <= *number->n )
+			printf( "%ld.argument => %ld\n", index, a );
+		
+		++index;
+	}
+}
+
+int main( void )
+{
+	// Structure(s)
+	struct numbers *num = ( struct numbers* )malloc( sizeof( struct numbers ) );
+
+	// Variable(s)
+	unsigned long int limit;
+	
+	printf( "Type here limit: " );
+	scanf( "%ld", &limit );
+	
+	num->n = &limit;
+
+	calculateFibbonachi( num );
+	free( num );
+
+	return ( EXIT_SUCCESS );
+}
